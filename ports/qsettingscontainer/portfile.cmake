efff5551f08812ffb58c00eb022547666162cc9e
Dan Keenan <dk@dankeenan.org>
2024-02-11 09:32:39 -0500

    Update qsettingscontainer.

--- ports/qsettingscontainer/portfile.cmake
+++ ports/qsettingscontainer/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO danielskeenan/QSettingsContainer
     REF v2.0.0
-    SHA512 daf8b997d1e58a0e7a13ef4bd8c1fd80533e017a0ecd2a09e7f9ef14546454756dfcb83149c437afed7e44030edc0acb7d85f7b71e2cb3ee58560909521bd321
+    SHA512 0f196e1f616ba26246cc19acbd960c54fe6e4b7f8e66226a8c8dc476d439e27b45a8600b68adab60a67912d2a466cfb90b49f0af52b426f355cb80fca51b31f4
     HEAD_REF master
 )
 
@@ -11,8 +11,11 @@ vcpkg_cmake_configure(
     OPTIONS
         -DQSETTINGSCONTAINER_BUILD_DOC=OFF
 )
-vcpkg_cmake_config_fixup()
 
 vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(
+        PACKAGE_NAME QSettingsContainer
+        CONFIG_PATH "share/cmake/QSettingsContainer"
+)
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
